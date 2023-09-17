1a9c45b7aeea17d1dabc49422671549e4227fd4d
SalmanMinhas15 <144421353+SalmanMinhas15@users.noreply.github.com>
2023-09-17 08:48:59 -0700

    Add files via upload

---
+++ pistol.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+system("color 03");
+cout<<"+--^---------,---------,-------,---------^-- ,        "<<endl;
+cout<<" |   ||||||    '--------'        |             O      "<<endl;
+cout<<" ' +-----------------------------^------------|     "<<endl;
+cout<<"    ',---------,--------,------------------- '      "<<endl;
+cout<<"      / xxxxx /'|        /'    "<<endl;
+cout<<"     / xxxxx /  |       /'     "<<endl;
+cout<<"    / xxxxx /' --------'      "<<endl;
+cout<<"   / xxxxx /      "<<endl;
+cout<<"  / xxxxx /      "<<endl;
+cout<<" (-------(     "<<endl;
+cout<<"  '-----'      ";
+      }           
